7ea22309f2f7f10204151269ef142a533b009b03
Kevin M Ashton <kevin.m.ashton@gmail.com>
2020-09-02 08:31:58 -0600

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { createFirestoreLift } from './FirestoreLift';
-export { BatchTask, FirestoreLiftInitConfig } from './models';
+export { BatchTask, FirestoreLiftInitConfig, OrNull } from './models';
 export { FirestoreLiftCollection } from './FirestoreLiftCollection';
 export { createRtdbLift, TypedFirebaseObjectOrPrimativeRefGenerator } from './RTDB';
